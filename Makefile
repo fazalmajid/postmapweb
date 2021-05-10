5e0585f456c8fa4caeb2ba535e2c8ad63aa991b2
Fazal Majid <fazal@majid.org>
2021-05-10 19:23:00 +0100

    updated HandsOnTable to 6.2.2, last MIT-licensed version

--- Makefile
+++ Makefile
@@ -4,33 +4,13 @@ all: postmapweb
 run: postmapweb
 	./postmapweb -v -p :8080 -c conf.json
 
-CSS=	handsontable/dist/handsontable.full.min.css
-JS=	handsontable/dist/handsontable.full.min.js templates/view.js
+CSS=	handsontable/dist/handsontable.full.css
+JS=	handsontable/dist/handsontable.full.js templates/view.js
 ASSETS=	$(CSS) $(JS) templates/view.html templates/error.html
 TEMPLATES= templates/view.html templates/error.html
 
-postmapweb: postmapweb.go middleware.go $(JS) $(CSS)
+postmapweb: postmapweb.go middleware.go $(ASSETS)
 	$(GO) build
-
-$(GODEPS):
-	$(GO) get ${@:src/%=%}
-
-src/golang.org/x/crypto/ssh/terminal:
-	-mkdir -p src/golang.org/x
-	git clone -q https://go.googlesource.com/crypto src/golang.org/x/crypto
-	cp patches/util_solaris.go src/golang.org/x/crypto/ssh/terminal
-	$(GO) get golang.org/x/crypto/ssh/terminal
-
-BOWER=	node_modules/bower/bin/bower
-$(BOWER):
-	npm install bower
-
-HANDSONTABLE=	bower_components/handsontable/dist
-$(JS) $(CSS):
-	$(MAKE) $(BOWER)
-	echo n | ./node_modules/bower/bin/bower install handsontable --save
-	mkdir -p handsontable/dist
-	cp $(HANDSONTABLE)/handsontable.full.min.* handsontable/dist
 
 profile: cpu.prof
 	echo top10|go tool pprof postmapweb cpu.prof
@@ -42,8 +22,8 @@ cpu.prof: postmapweb
 	./postmapweb -p :8080 -c conf.cpu -d temboz.com -m virtual.cpu -w sopo
 	./postmapweb -p :8080 -c conf.cpu -cpuprofile cpu.prof &
 	sleep 3
-	ab -n 10 -A temboz.com:sopo 'http://localhost:8080/handsontable/handsontable.full.min.css'
-#	ab -n 10 -A temboz.com:sopo 'http://localhost:8080/handsontable/handsontable.full.min.js'
+	ab -n 10 -A temboz.com:sopo 'http://localhost:8080/handsontable/handsontable.full.css'
+#	ab -n 10 -A temboz.com:sopo 'http://localhost:8080/handsontable/handsontable.full.js'
 #	ab -n 10 -A temboz.com:sopo 'http://localhost:8080/'
 	pkill -HUP postmapweb
 	sleep 1
--- templates/view.js
+++ templates/view.js
@@ -1,95 +1,96 @@
 var data = {{.Aliases}};
 var changes = {};
 function add_change(cl, s) {
-  var n = document.createElement("li");
-  var n2 = document.createTextNode(s);
-  n.appendChild(n2);
-  cl.appendChild(n);
+    var n = document.createElement("li");
+    var n2 = document.createTextNode(s);
+    n.appendChild(n2);
+    cl.appendChild(n);
 }
 function show_changes() {
-  var cl = document.getElementById("changelog");
-  var l = [];
-  cl.innerHTML = "";
-  for (var cell in changes) {
-    if (changes[cell]["was"] != null && changes[cell]["was"] != changes[cell]["is"]) {
-      add_change(cl, "removed " + changes[cell]["was"]);
-      l.push({"op": "remove",
-              "alias": changes[cell]["was"]});
+    var cl = document.getElementById("changelog");
+    var l = [];
+    cl.innerHTML = "";
+    for (var cell in changes) {
+        if (changes[cell]["was"] != null && changes[cell]["was"] != changes[cell]["is"]) {
+            add_change(cl, "removed " + changes[cell]["was"]);
+            l.push({"op": "remove",
+                    "alias": changes[cell]["was"]});
+        }
+        add_change(cl, changes[cell]["is"] + " \u2192 " + changes[cell]["target"]);
+        l.push({"op": "add",
+                "alias": changes[cell]["is"],
+                "target": changes[cell]["target"]});
     }
-    add_change(cl, changes[cell]["is"] + " \u2192 " + changes[cell]["target"]);
-    l.push({"op": "add",
-            "alias": changes[cell]["is"],
-            "target": changes[cell]["target"]});
-  }
-  document.forms[0].changes.value = JSON.stringify(l);
+    document.forms[0].changes.value = JSON.stringify(l);
 }
 function change_handler(change, source) {
-  if (source === 'loadData') {
-    return; //don't save this change
-  }
-  var i;
-  for(i=0; i<change.length; i++) {
-    var cell = change[i][0];
-    var prop = change[i][1];
-    var oldVal = change[i][2];
-    var newVal = change[i][3];
-    if (prop == 0) {
-      if (changes[cell] == null) {
-        if (oldVal == null) {
-          changes[cell] = {"was": newVal};
-        } else {
-          changes[cell] = {"was": oldVal};
-        }
-        changes[cell]["is"] = newVal;
-        changes[cell]["target"] = data[cell][1];
-      }
-      if (newVal.indexOf("@{{.Domain}}") == -1) {
-        hot.getCell(cell, 0).style.backgroundColor = "#fc9";
-        document.getElementById("submit").disabled = true;
-        errored[cell] = true;
-      } else {
-        hot.getCell(cell, 0).style.backgroundColor = "#fff";
-        if (errored[cell]) {
-          delete errored[cell];
-          hot.getCell(cell, 0).style.backgroundColor = "#fff";
-          if (Object.keys(errored).length == 0) {
-            document.getElementById("submit").disabled = false;
-          }
-        }
-      }
-    } else {
-      if (changes[cell] == null) {
-        changes[cell] = {"was": data[cell][0], "is": data[cell][0]};
-      }
-      changes[cell]["target"] = newVal;
+    if (source === 'loadData') {
+        return; //don't save this change
     }
-  }
-  show_changes();
+    var i;
+    for(i=0; i<change.length; i++) {
+        var cell = change[i][0];
+        var prop = change[i][1];
+        var oldVal = change[i][2];
+        var newVal = change[i][3];
+        if (prop == 0) {
+            if (changes[cell] == null) {
+                if (oldVal == null) {
+                    changes[cell] = {"was": newVal};
+                } else {
+                    changes[cell] = {"was": oldVal};
+                }
+                changes[cell]["is"] = newVal;
+                changes[cell]["target"] = data[cell][1];
+            }
+            if (newVal.indexOf("@{{.Domain}}") == -1) {
+                hot.getCell(cell, 0).style.backgroundColor = "#fc9";
+                document.getElementById("submit").disabled = true;
+                errored[cell] = true;
+            } else {
+                hot.getCell(cell, 0).style.backgroundColor = "#fff";
+                if (errored[cell]) {
+                    delete errored[cell];
+                    hot.getCell(cell, 0).style.backgroundColor = "#fff";
+                    if (Object.keys(errored).length == 0) {
+                        document.getElementById("submit").disabled = false;
+                    }
+                }
+            }
+        } else {
+            if (changes[cell] == null) {
+                changes[cell] = {"was": data[cell][0], "is": data[cell][0]};
+            }
+            changes[cell]["target"] = newVal;
+        }
+    }
+    show_changes();
 }
 function check_pending() {
-  var e = hot.getActiveEditor();
-  if (e.state == "STATE_EDITING" && e.TEXTAREA.value != e.originalValue) {
-    change_handler([[e.row, e.col, e.originalValue, e.TEXTAREA.value]],
-                   "pending");
-  }
-  return true;
+    var e = hot.getActiveEditor();
+    if (e.state == "STATE_EDITING" && e.TEXTAREA.value != e.originalValue) {
+        change_handler([[e.row, e.col, e.originalValue, e.TEXTAREA.value]],
+                       "pending");
+    }
+    return true;
 }
+
 //document.getElementById("submit").onsubmit = check_pending;
 var errored = {};
 var hot;
 function onload_handler() {
-  var container = document.getElementById('spreadsheet');
-  hot = new Handsontable(container, {
-    data: data,
-    minSpareRows: 1,
-    rowHeaders: true,
-    colHeaders: ["Email alias", "Destination(s)"],
-    contextMenu: true,
-    afterChange: change_handler
-  });
+    var container = document.getElementById('spreadsheet');
+    hot = new Handsontable(container, {
+        data: data,
+        minSpareRows: 1,
+        rowHeaders: true,
+        colHeaders: ["Email alias", "Destination(s)"],
+        contextMenu: false,
+        afterChange: change_handler,
+    });
 }
 if ( "complete" == document.readyState ) {
-  onload_handler();
+    onload_handler();
 } else {
     window.addEventListener("load", onload_handler, false);
 }
